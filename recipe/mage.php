b4f89d61e2975e304082d1274fa19e13aa5a274f
Michael Woodward <mikeymike.mw@gmail.com>
2018-02-05 21:52:23 +0000

    Add SSH Multiplexing patch to rsync

--- recipe/mage.php
+++ recipe/mage.php
@@ -69,3 +69,43 @@ task('release:upgrade', [
 ]);
 
 after('deploy:failed', 'deploy:unlock');
+
+/**
+ * Patch rsync recipe SSH Args until...
+ * @see https://github.com/deployphp/deployer/pull/1531
+ */
+task('rsync', function () {
+    $config = get('rsync');
+    $src = get('rsync_src');
+    while (is_callable($src)) {
+        $src = $src();
+    }
+    if (!trim($src)) {
+        // if $src is not set here rsync is going to do a directory listing
+        // exiting with code 0, since only doing a directory listing clearly
+        // is not what we want to achieve we need to throw an exception
+        throw new \RuntimeException('You need to specify a source path.');
+    }
+    $dst = get('rsync_dest');
+    while (is_callable($dst)) {
+        $dst = $dst();
+    }
+    if (!trim($dst)) {
+        // if $dst is not set here we are going to sync to root
+        // and even worse - depending on rsync flags and permission -
+        // might end up deleting everything we have write permission to
+        throw new \RuntimeException('You need to specify a destination path.');
+    }
+    /** @var \Deployer\Host\Host $server */
+    $server = \Deployer\Task\Context::get()->getHost();
+    if ($server instanceof \Deployer\Host\Localhost) {
+        runLocally("rsync -{$config['flags']} {{rsync_options}}{{rsync_excludes}}{{rsync_includes}}{{rsync_filter}} '$src/' '$dst/'", $config);
+        return;
+    }
+    $host = $server->getRealHostname();
+    $port = $server->getPort() ? ' -p' . $server->getPort() : '';
+    $sshArguments = $server->getSshArguments();
+    $sshArguments = $server->isMultiplexing() ? $sshArguments->withMultiplexing($server) : $sshArguments;
+    $user = !$server->getUser() ? '' : $server->getUser() . '@';
+    runLocally("rsync -{$config['flags']} -e 'ssh$port $sshArguments' {{rsync_options}}{{rsync_excludes}}{{rsync_includes}}{{rsync_filter}} '$src/' '$user$host:$dst/'", $config);
+});
