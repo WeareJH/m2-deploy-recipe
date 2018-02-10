3d41f874881fe7deb117dc2c97f24f49f924ff42
Michael Woodward <mikeymike.mw@gmail.com>
2018-02-10 15:37:43 +0000

    Remove rsync

--- recipe/config.php
+++ recipe/config.php
@@ -26,47 +26,6 @@ set('writable_dirs', [
     'pub/static',
 ]);
 
-set('rsync_src', get('local_src'));
-set('rsync', [
-    'exclude'       => [
-        '/.docker',
-        '/docker-compose*.yml',
-        '/*.dockerfile',
-        '/.dockerignore',
-        '/.git',
-        '/.gitignore',
-        '/deploy.php',
-        '/dev',
-        '/test',
-        '/phpserver',
-        '/LICENSE*.txt',
-        '/COPYING.txt',
-        '/.user.ini',
-        '/.travis.yml',
-        '/.php_cs',
-        '/.htaccess*',
-        '/*.sample',
-        '/.phpstorm.meta.php',
-        '/*.md',
-        '/circle.yml',
-        '/.idea',
-        '/phpunit.xml',
-        '/phpcs.xml',
-        '/auth.json',
-        '/app/etc/env.php',
-        'node_modules'
-    ],
-    'exclude-file'  => false,
-    'include'       => [],
-    'include-file'  => false,
-    'filter'        => [],
-    'filter-file'   => false,
-    'filter-perdir' => false,
-    'flags'         => 'rzL',
-    'options'       => ['delete'],
-    'timeout'       => 3600,
-]);
-
 set('local_bin/php', function () {
     return runLocally('which php');
 });
--- recipe/mage.php
+++ recipe/mage.php
@@ -9,6 +9,8 @@ require 'recipe/rsync.php';
 require __DIR__ . '/config.php';
 // Upgrade Deployments
 require __DIR__ . '/custom/deploy.php';
+// Zip Deployments
+require __DIR__ . '/custom/zip.php';
 // Custom Magento Commands
 require __DIR__ . '/custom/magento2.php';
 require __DIR__ . '/custom/magentoInstall.php';
@@ -24,7 +26,9 @@ task('deploy', [
     'deploy:prepare',
     'deploy:lock',
     'deploy:release',
-    'rsync',
+    'deploy:zip:create',
+    'deploy:zip:upload',
+    'deploy:zip:unzip',
     'deploy:unlock',
     'success'
 ]);
@@ -68,43 +72,3 @@ task('release:backup', [
 ]);
 
 after('deploy:failed', 'deploy:unlock');
-
-/**
- * Patch rsync recipe SSH Args until...
- * @see https://github.com/deployphp/deployer/pull/1531
- */
-task('rsync', function () {
-    $config = get('rsync');
-    $src = get('rsync_src');
-    while (is_callable($src)) {
-        $src = $src();
-    }
-    if (!trim($src)) {
-        // if $src is not set here rsync is going to do a directory listing
-        // exiting with code 0, since only doing a directory listing clearly
-        // is not what we want to achieve we need to throw an exception
-        throw new \RuntimeException('You need to specify a source path.');
-    }
-    $dst = get('rsync_dest');
-    while (is_callable($dst)) {
-        $dst = $dst();
-    }
-    if (!trim($dst)) {
-        // if $dst is not set here we are going to sync to root
-        // and even worse - depending on rsync flags and permission -
-        // might end up deleting everything we have write permission to
-        throw new \RuntimeException('You need to specify a destination path.');
-    }
-    /** @var \Deployer\Host\Host $server */
-    $server = \Deployer\Task\Context::get()->getHost();
-    if ($server instanceof \Deployer\Host\Localhost) {
-        runLocally("rsync -{$config['flags']} {{rsync_options}}{{rsync_excludes}}{{rsync_includes}}{{rsync_filter}} '$src/' '$dst/'", $config);
-        return;
-    }
-    $host = $server->getRealHostname();
-    $port = $server->getPort() ? ' -p' . $server->getPort() : '';
-    $sshArguments = $server->getSshArguments();
-    $sshArguments = $server->isMultiplexing() ? $sshArguments->withMultiplexing($server) : $sshArguments;
-    $user = !$server->getUser() ? '' : $server->getUser() . '@';
-    runLocally("rsync -{$config['flags']} -e 'ssh$port $sshArguments' {{rsync_options}}{{rsync_excludes}}{{rsync_includes}}{{rsync_filter}} '$src/' '$user$host:$dst/'", $config);
-});
