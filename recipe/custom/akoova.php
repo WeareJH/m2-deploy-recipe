8d7152dc529fe5275198a1f6cc3dd930308ed5a1
Michael Woodward <mikeymike.mw@gmail.com>
2018-06-04 12:23:02 +0100

    Import required classes for Akoova zip upload

--- recipe/custom/akoova.php
+++ recipe/custom/akoova.php
@@ -2,6 +2,9 @@
 
 namespace Deployer;
 
+use Deployer\Ssh\Arguments;
+use Deployer\Task\Context;
+
 task('akoova:zip:upload', function () {
     $server = Context::get()->getHost();
     $sshPort = $server->getPort();
